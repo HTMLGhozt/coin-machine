0fdcd321045c144026f5a506accb7a1860908519
Thomas Dillard <thomas.dillard.studios@gmail.com>
2020-07-31 17:28:58 -0700

    Added formatting
    - Using husky lint-staged and prettier to format on commit

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -2,12 +2,12 @@ import Koa from 'koa';
 
 const app = new Koa();
 
-app.use(async function(ctx) {
-  ctx.body = 'Hello World';
+app.use(async function (ctx) {
+	ctx.body = 'Hello World';
 });
 
 const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
-    console.info(`Running on port: ${port}`)
+	console.info(`Running on port: ${port}`);
 });
