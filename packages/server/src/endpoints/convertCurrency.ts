392e5b312ff87666ca214708ff6fc625cffe185b
Thomas Dillard <thomas.dillard.studios@gmail.com>
2020-08-01 03:00:35 -0700

    Tighter restrictions on format on server.

--- packages/server/src/endpoints/convertCurrency.ts
+++ packages/server/src/endpoints/convertCurrency.ts
@@ -12,7 +12,7 @@ router.post('/convert-currency', (ctx): void => {
 	// multiplying the currency by 100 avoids problems with floating point math
 	// https://0.30000000000000004.com/
 	const total = currencyToFloat(currency) * 100;
-	if (typeof total !== 'number' || Number.isNaN(total)) {
+	if (Number.isNaN(total)) {
 		ctx.throw(422, { error: "Currency couldn't be converted to a number." });
 	}
 
