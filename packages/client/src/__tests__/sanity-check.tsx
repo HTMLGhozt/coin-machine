147b1b807e11ec9824de5d01f52bb07c19d4210e
Thomas Dillard <thomas.dillard.studios@gmail.com>
2020-07-31 18:08:49 -0700

    Setup for ts-jest and @testing-library/react

---
+++ packages/client/src/__tests__/sanity-check.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import App from '../App';
+
+test('ts-jest works', () => {});
+
+test('App.tsx renders without errors', () => {
+	render(<App />);
+});
