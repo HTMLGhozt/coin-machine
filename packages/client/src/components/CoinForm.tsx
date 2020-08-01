80133c9002028a549f11a92978b3d4946fd392ad
Thomas Dillard <thomas.dillard.studios@gmail.com>
2020-08-01 02:54:09 -0700

    Better formatting of currency input.

--- packages/client/src/components/CoinForm.tsx
+++ packages/client/src/components/CoinForm.tsx
@@ -1,37 +1,48 @@
 import React, { useContext } from 'react';
 import { useForm } from 'react-hook-form';
+import formatCurrency from '../utils/formatCurrency';
 import getError from '../utils/getError';
 import { CoinContext } from './DisplayContainer';
 
 export default function CoinForm() {
-	const { register, handleSubmit, errors } = useForm();
+	const { register, handleSubmit, errors, getValues, setValue } = useForm();
 	const { getCoinsFromAmount } = useContext(CoinContext);
+
 	const errorMessage = getError({ name: 'currency', errors });
+	const formatCurrencyOnEvent = () => {
+		const currency = getValues('currency');
+		if (currency) {
+			setValue('currency', formatCurrency(currency));
+		}
+	}
 
 	return (
 		<form
-			onSubmit={handleSubmit(({ currency }) => (getCoinsFromAmount(currency)))}
+			onSubmit={handleSubmit(({ currency }) => {
+				formatCurrencyOnEvent();
+				getCoinsFromAmount(currency);
+			})}
 		>
 			<label htmlFor="currency">Dollar Amount</label>
 			<input
 				id="currency"
 				ref={register({
 					required: true,
-					// TODO::073020 Is pattern matching required for input?
+					// TODO::073120 Is pattern matching required for input?
+					// TODO::080120 If I18n is required this'll need to change
 					pattern: {
-						value: /[0-9,.]*/,
+						value: /^\$?[0-9,]*(\.[0-9]+)?$/,
 						message:
 							'The input must be formatted like a currency (ex $100.00 or 100 or 1000)',
 					},
 				})}
+				onBlur={formatCurrencyOnEvent}
 				autoComplete="transaction-amount"
 				inputMode="numeric"
 				name="currency"
 			/>
-			{errorMessage ? (
-				<p>{errorMessage}</p>
-			) : null}
-			<button type="submit">submit</button>
+			{errorMessage ? <p>{errorMessage}</p> : null}
+			<button disabled={!!errorMessage} type="submit">submit</button>
 		</form>
 	);
 }
--- packages/server/src/endpoints/convertCurrency.ts
+++ packages/server/src/endpoints/convertCurrency.ts
@@ -1,5 +1,6 @@
 import Router from '@koa/router';
 import amountToCoins from '../utils/amountToCoins';
+import currencyToFloat from '../utils/currencyToFloat';
 
 const router = new Router({
 	prefix: '/api'
@@ -8,7 +9,9 @@ const router = new Router({
 router.post('/convert-currency', (ctx): void => {
 	const { currency } = ctx.request.body;
 
-	const total = +currency * 100;
+	// multiplying the currency by 100 avoids problems with floating point math
+	// https://0.30000000000000004.com/
+	const total = currencyToFloat(currency) * 100;
 	if (typeof total !== 'number' || Number.isNaN(total)) {
 		ctx.throw(422, { error: "Currency couldn't be converted to a number." });
 	}
