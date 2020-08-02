4fac1e8ad37a7e6a1e0dff65be67295da5d7da4e
Thomas Dillard <thomas.dillard.studios@gmail.com>
2020-08-01 17:37:57 -0700

    frontend unit testing

---
+++ packages/client/src/components/__tests__/CoinList.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { CoinContext } from '../DisplayContainer';
+import CoinList from '../CoinList';
+
+const testList = { 
+	"silver-dollar": 0,
+	"half-dollar": 1,
+	"quarter": 1,
+	"dime": 2,
+	"nickel": 0,
+	"penny": 4
+};
+
+test('Given a list of records it should render all of them', () => {
+	const wrapper = (
+		<CoinContext.Provider
+			value={{ getCoinsFromAmount: () => {}, coinMap: { ...testList } }}
+		>
+			<CoinList />
+		</CoinContext.Provider>
+	);
+	const { getByText } = render(wrapper);
+
+	Object.entries(testList).forEach(([key, value]) => {
+		const lineItem = getByText(new RegExp(key, 'i'));
+		expect(lineItem).toBeInTheDocument();
+		expect(lineItem).toHaveTextContent(`${value}`);
+	});
+});
